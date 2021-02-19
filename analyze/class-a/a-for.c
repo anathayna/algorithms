826f9658ac6c5badb5d17a32dd91cd7a753a5520
ana thayna <anathaynafranca@gmail.com>
2021-02-19 10:05:53 -0300

    adding for & while

---
+++ analyze/class-a/a-for.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void) { 
+    int n = 4;
+    for (int i = 0; i*i < n; i++){        
+        printf("teste");
+    }
+}
+
+// linha 2
+// n = 4, o laço executa 2 vezes
+// n = 8, o laço executa 3 vezes
+// n = 16, o laço executa 4 vezes
+
+// quando o laço pára ?  quando i*i >= n
+
+// i*i = n
+
+// qual é o valor de i quando o laço pára ?
+// i = raiz_quadrada(n)
