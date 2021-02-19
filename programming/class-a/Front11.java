53c48b55a0c70dc173d0f1ffa48b03849e8950dd
anathayna <anathaynafranca@gmail.com>
2021-02-19 19:55:54 -0300

    start front11 exercice

---
+++ programming/class-a/Front11.java
@@ -0,0 +1,16 @@
+class Front11 {
+    public static void main(String[] args) { 
+        int x[] = {1, 2, 3};
+        int y[] = {7, 9, 8};
+        System.out.println(front11(x,y));
+        // front11([1, 2, 3], [7, 9, 8]) → [1, 7]
+        // front11([1], [2]) → [1, 2]
+        // front11([1, 7], []) → [1]
+    }
+
+    public static int[] front11(int[] a, int[] b) {
+  
+    }
+}
+
+// build & run java Front11.java
