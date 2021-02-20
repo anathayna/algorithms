a68c947d031793cc3408149f1f205722e7867627
anathayna <anathaynafranca@gmail.com>
2021-02-20 13:54:23 -0300

    finish front11 exercice

--- programming/class-a/Front11.java
+++ programming/class-a/Front11.java
@@ -9,7 +9,17 @@ class Front11 {
     }
 
     public static int[] front11(int[] a, int[] b) {
-  
+        if (a.length == 0 && b.length == 0)
+            return new int[] {};
+   
+        else if (a.length !=0 && b.length == 0)
+            return new int[]{a[0]};
+
+        else if (a.length == 0 && b.length != 0)
+            return new int[]{b[0]};
+
+        else
+            return new int[] {a[0],b[0]};
     }
 }
 
