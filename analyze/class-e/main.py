bc1421bd5f3c315cfd9d897e029a084107e61049
ana thayna <anathaynafranca@gmail.com>
2021-04-10 23:59:13 -0300

    finish exercice

--- analyze/class-e/main.py
+++ analyze/class-e/main.py
@@ -1,21 +1,35 @@
-from sys import maxint
-
-def maxSumSegment(v,size):
-      
-    max_segment = -maxint - 1
-    max_end = 0
-      
-    for i in range(0, size):
-        max_end = max_end + v[i]
-        if (max_segment < max_end):
-            max_segment = max_end
+def maxLeftRightSum(v, l, m, h):
+    sum = 0
+    left = -10000
+    for i in range(m, l-1, -1):
+        sum = sum + v[i]
+        if (sum > left):
+            left = sum
  
-        if max_end < 0:
-            max_end = 0  
-    return max_segment
+    sum = 0
+    right = -1000
+    for i in range(m + 1, h + 1):
+        sum = sum + v[i]
+        if (sum > right):
+            right = sum
+
+    return max(left + right, left, right)
+ 
+
+def maxSumSegment(v, l, h):
+    if (l == h):
+        return v[l]
+ 
+    mid = (l + h) // 2
+
+    return max(maxSumSegment(v, l, mid),
+               maxSumSegment(v, mid+1, h),
+               maxLeftRightSum(v, l, mid, h))
+
 
 v = [-16, 20, -10, 12, 27, -6, -4, 8]
-print "max sum segment:", maxSumSegment(v, len(v))
+n = len(v)
+print "max contiguous sum:", maxSumSegment(v, 0, n-1)
 
 # input -16, 20, -10, 12, 27, -6, -4, 8
 # output 1 4 49
