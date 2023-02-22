018522a3064b2947084857fc3f728d5fb1c33c25
ana thayna <ana.thayna@enjoei.com.br>
2023-02-22 17:03:49 -0300

    :tada: start(algorithms): adding karatsuba algorithm

---
+++ 0x04 design & analysis/karatsuba.c
@@ -0,0 +1,13 @@
+/*
+fast multiplication algorithm
+divide-and-conquer algorithm
+reduces the multiplication of 2 n-digit numbers to 3 multiplications of n/2-digit numbers
+*/
+
+#include <stdio.h>
+#include <math.h>
+
+int main(void) {
+  printf("karatsuba multiplication algorithm!\n");
+  return 0;
+}
